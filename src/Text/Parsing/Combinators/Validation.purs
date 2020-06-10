c63bbc1d123f3eea20667aa4c52eb108bfc93b7b
Mark Farrell <markfarr2011@gmail.com>
2020-06-10 20:44:28 +0000

    + and,or,not,input,output,success,failure,left,right,just,nothing

--- src/Text/Parsing/Combinators/Validation.purs
+++ src/Text/Parsing/Combinators/Validation.purs
@@ -1,17 +1,23 @@
 module Text.Parsing.Combinators.Validation
-  ( succeed
-  , fail
+  ( and
   , or
-  , and
-  , left
-  , right
+  , not
   , input
   , output
+  , success
+  , failure
+  , left
+  , right
+  , just
+  , nothing
   ) where
 
 import Prelude
 
-import Control.Monad.State.Class (gets)
+import Control.Monad.Except.Trans (ExceptT)
+import Control.Monad.Except.Trans as E
+
+import Control.Monad.State.Class as S
 import Control.Monad.Trans.Class (lift)
 
 import Data.Either (Either(..))
@@ -22,44 +28,44 @@ import Text.Parsing.Parser (ParserT)
 import Text.Parsing.Parser as P
 import Text.Parsing.Parser.Combinators as C
 
+-- | Accepts a default value of `Nothing` in the case that the parser `p` fails when applied to the remaining parse input.
+-- | Does not consume the remaiing parse input.
+maybe :: forall a m b. Monad m => ParserT a m b -> ParserT a m (Maybe b)
+maybe = C.optionMaybe <<< C.lookAhead
+
 -- | Accepts a default value `x` in in the case that the parser `p` fails when applied to the remaining parse input.
--- | Consumes the remaining parse input.
-accept :: forall a m b. Monad m => m b -> ParserT a m b -> ParserT a m b
-accept = \x p -> do
-  y <- C.optionMaybe $ p
+-- | Does not consume the remaining parse input.
+succeedWith :: forall a m b. Monad m => m b -> ParserT a m b -> ParserT a m b
+succeedWith = \x p -> do
+  y <- maybe p
   case y of
     (Just z)  -> pure z
     (Nothing) -> lift x
 
 -- | Accepts a default value `x` in in the case that the parser `p` fails when applied to the remaining parse input.
 -- | Fails if the result of applying the parser `p` to the remaining parse input is successful.
--- | Consumes the remaining parse input if `p` is successful.
-reject :: forall a m b. Monad m => m b -> ParserT a m b -> ParserT a m b
-reject = \x p -> do
-  y <- C.optionMaybe p
+-- | Does not consume the remaining parse input if `p` is successful.
+failWith :: forall a m b. Monad m => m b -> ParserT a m b -> ParserT a m b
+failWith = \x p -> do
+  y <- maybe p
   case y of
     (Just _)  -> P.fail $ "Validation failure: rejected successful parse result."
     (Nothing) -> lift x
 
--- | Accepts a default value `x` in in the case that the parser `p` fails when applied to the remaining parse input.
--- | Does not consume the remaining parse input.
-succeed :: forall a m b. Monad m => m b -> ParserT a m b -> ParserT a m b
-succeed = \x p -> accept x $ C.lookAhead p
-
 -- | Fails if the result of applying the parser `p` to the remaining parse input is successful.
 -- | Does not consume the remaining parse input.
-fail :: forall a m b. Monad m => ParserT a m b -> ParserT a m Unit
-fail = \p -> reject (pure unit) (C.lookAhead p *> pure unit)
+not :: forall a m b. Monad m => ParserT a m b -> ParserT a m Unit
+not = \p -> failWith (pure unit) (p *> pure unit)
 
 -- | Attempts to validate the remaining parse input with a parser `q`, or a parser `p` if `q` fails.
 -- | Fails if the validation of the remaining parse input with parsers `p` or `q` is unsuccessful.
 -- | Does not consume the remaining parse input.
 or :: forall a m b c. Monad m => ParserT a m b -> ParserT a m c -> ParserT a m (Either b c)
 or = \p q -> do
-  x <- C.optionMaybe $ C.lookAhead q
+  x <- maybe q
   case x of
     (Nothing) -> do
-      y <- C.optionMaybe $ C.lookAhead p
+      y <- maybe p
       case y of
         (Nothing) -> P.fail $ "Validation failure: both validation attempts were unsuccessful."
         (Just z)  -> pure $ Left z
@@ -70,8 +76,8 @@ or = \p q -> do
 -- | Does not consume the remaining parse input.
 and :: forall a m b c. Monad m => ParserT a m b -> ParserT a m c -> ParserT a m (Tuple b c)
 and = \p q -> do
-  w <- C.optionMaybe $ C.lookAhead p
-  x <- C.optionMaybe $ C.lookAhead q
+  w <- maybe p
+  x <- maybe q
   case (Tuple w x) of
     (Tuple (Nothing) (Just _))  -> P.fail $ "Validation failure: first validation attempt was unsuccessful."
     (Tuple (Just _)  (Nothing)) -> P.fail $ "Validation failure: second validation attempt was unsuccessful."
@@ -93,7 +99,7 @@ right (Right x) = pure x
 -- | Lifts the remaining parse input for validation purposes.
 -- | Does not consume the remaining parse input.
 getInput :: forall a m. Monad m => ParserT a m a
-getInput = gets \(P.ParseState x _ _) -> x 
+getInput = S.gets \(P.ParseState x _ _) -> x
 
 -- | Runs a parser `p` against the remaining parse input for validation purposes.
 -- | Does not consume the remaining parse input.
@@ -106,7 +112,7 @@ input = \p -> do
   pure z
 
 -- | Runs a parser `p` against the remaining parse input, and `q` against the result if successful.
--- | Does not consume the remaining parse input. 
+-- | Does not consume the remaining parse input.
 output :: forall a m b c. Monad m => ParserT a m b -> ParserT b m c -> ParserT a m c
 output = \p q -> do
   w <- input p
@@ -114,3 +120,31 @@ output = \p q -> do
   y <- lift x
   z <- right y
   pure z
+
+-- | Fails if the parsed result throws an exception when run.
+-- | Dones not consume remaining parse input
+success :: forall a m b c. Monad m => ExceptT a m b -> ParserT c m b
+success = \w -> do
+  x <- lift $ E.runExceptT w
+  y <- right x
+  pure y
+
+-- | Fails if the parsed result throws is successful when run.
+-- | Dones not consume remaining parse input
+failure :: forall a m b c. Monad m => ExceptT a m b -> ParserT c m a
+failure = \w -> do
+  x <- lift $ E.runExceptT w
+  y <- left x
+  pure y
+
+-- | Fails if the parsed result is nothing.
+-- | Does not consume the remaining parse input.
+just :: forall a m b. Monad m => (Maybe b) -> ParserT a m b
+just (Nothing)  = P.fail $ "Validation failure: did not expect nothing result."
+just (Just x)   = pure x
+
+-- | Fails if the parsed result is not nothing.
+-- | Does not consume the remaining parse input.
+nothing :: forall a m b. Monad m => (Maybe b) -> ParserT a m Unit
+nothing (Just _)  = P.fail $ "Validation failure: expected nothing result."
+nothing (Nothing) = pure unit
